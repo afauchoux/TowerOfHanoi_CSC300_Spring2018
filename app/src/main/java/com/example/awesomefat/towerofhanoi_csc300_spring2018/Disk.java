dd4f140e1a8e2ede3ffa896eadb47e8ccb2f06aa
adamfauchoux <adam.fauchoux@cuw.edu>
2018-02-19 22:50:30 -0600

    first push

--- app/src/main/java/com/example/awesomefat/towerofhanoi_csc300_spring2018/Disk.java
+++ app/src/main/java/com/example/awesomefat/towerofhanoi_csc300_spring2018/Disk.java
@@ -15,15 +15,18 @@ public class Disk
         this.nextDisk = null;
     }
 
-    public int getSize() {
+    public int getSize()
+    {
         return size;
     }
 
-    public Disk getNextDisk() {
+    public Disk getNextDisk()
+    {
         return nextDisk;
     }
 
-    public void setNextDisk(Disk nextDisk) {
+    public void setNextDisk(Disk nextDisk)
+    {
         this.nextDisk = nextDisk;
     }
 }
--- app/src/main/java/com/example/awesomefat/towerofhanoi_csc300_spring2018/MainActivity.java
+++ app/src/main/java/com/example/awesomefat/towerofhanoi_csc300_spring2018/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.awesomefat.towerofhanoi_csc300_spring2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button tower0Button;
+    private Button tower1Button;
+    private Button tower2Button;
+
+    private Tower tower0 = new Tower();
+    private Tower tower1 = new Tower();
+    private Tower tower2 = new Tower();
+
+    private Disk disk0 = new Disk(1);
+    private Disk disk1 = new Disk(2);
+    private Disk disk2 = new Disk(3);
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tower0Button = (Button)this.findViewById(R.id.tower0Button);
+        this.tower1Button = (Button)this.findViewById(R.id.tower1Button);
+        this.tower2Button = (Button)this.findViewById(R.id.tower2Button);
+
+    }
+
+    public void buttonOnClick (View v)
+    {
+        if(v == this.tower0Button)
+        {
+            System.out.println("Button 0");
+        }
+        else if(v == this.tower1Button)
+        {
+            System.out.println("Button 1");
+        }
+        else if(v == this.tower2Button)
+        {
+            System.out.println("Button 2");
+        }
     }
 }
